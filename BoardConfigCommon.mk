4be7970af03c8a58c7782d6094b2febd39213063
dianlujitao <dianlujitao@lineageos.org>
2021-02-10 16:48:22 +0800

    marlin: Workaround broken softAP with dynamic interface creation
    Change-Id: Iec8b34326a542b2dcde4c49544771d8e90d138ad

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,3 +8,6 @@ BUILD_BROKEN_DUP_RULES := true
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_CONFIG := marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
+
+# Wi-Fi
+WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
